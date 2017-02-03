26ea8d0d2a8b6af34c096790890d4b0654055608
tbarlow12 <tanner.barlow12@gmail.com>
2017-02-03 10:17:52 -0700

    Copied tutorial streaming for twitter

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+#Tutorial on Twitter Streaming from http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
+
 #Tanner's keys
 consumer_key = 'tgxm3fkzxyyWFvEwFppZrsN3p'
 consumer_secret = 'R4OGuyMn0XppBmahsDvcuqrwnCykyh7JCRmxBTlhhzw6RZTJ9Z'
@@ -18,6 +22,20 @@ class StdOutListener(StreamListener):
 
     def on_error(self, status):
         print status
+
+def word_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+    if match:
+        return True
+    return False
+def extract_link(text):
+    regex = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
+    match = re.search(regex, text)
+    if match:
+        return match.group()
+    return ''
 
 def main():
      #This handles Twitter authetification and the connection to Twitter Streaming API
@@ -38,6 +56,96 @@ def main():
             tweets_data.append(tweet)
         except:
             continue
+
+    print len(tweets_data)
+
+    tweets = pd.DataFrame()
+
+    tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+    tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
+    tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
+
+
+    tweets_by_lang = tweets['lang'].value_counts()
+
+    fig, ax = plt.subplots()
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=10)
+    ax.set_xlabel('Languages', fontsize=15)
+    ax.set_ylabel('Number of tweets' , fontsize=15)
+    ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+    tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+
+    tweets_by_country = tweets['country'].value_counts()
+
+    fig, ax = plt.subplots()
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=10)
+    ax.set_xlabel('Countries', fontsize=15)
+    ax.set_ylabel('Number of tweets' , fontsize=15)
+    ax.set_title('Top 5 countries', fontsize=15, fontweight='bold')
+    tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
+
+
+    tweets['python'] = tweets['text'].apply(lambda tweet: word_in_text('python', tweet))
+    tweets['javascript'] = tweets['text'].apply(lambda tweet: word_in_text('javascript', tweet))
+    tweets['ruby'] = tweets['text'].apply(lambda tweet: word_in_text('ruby', tweet))
+
+    print tweets['python'].value_counts()[True]
+    print tweets['javascript'].value_counts()[True]
+    print tweets['ruby'].value_counts()[True]
+
+    prg_langs = ['python', 'javascript', 'ruby']
+    tweets_by_prg_lang = [tweets['python'].value_counts()[True], tweets['javascript'].value_counts()[True], tweets['ruby'].value_counts()[True]]
+
+    x_pos = list(range(len(prg_langs)))
+    width = 0.8
+    fig, ax = plt.subplots()
+    plt.bar(x_pos, tweets_by_prg_lang, width, alpha=1, color='g')
+
+    # Setting axis labels and ticks
+    ax.set_ylabel('Number of tweets', fontsize=15)
+    ax.set_title('Ranking: python vs. javascript vs. ruby (Raw data)', fontsize=10, fontweight='bold')
+    ax.set_xticks([p + 0.4 * width for p in x_pos])
+    ax.set_xticklabels(prg_langs)
+    plt.grid()
+
+    tweets['programming'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet))
+    tweets['tutorial'] = tweets['text'].apply(lambda tweet: word_in_text('tutorial', tweet))
+
+    tweets['relevant'] = tweets['text'].apply(lambda tweet: word_in_text('programming', tweet) or word_in_text('tutorial', tweet))
+
+    print tweets['programming'].value_counts()[True]
+    print tweets['tutorial'].value_counts()[True]
+    print tweets['relevant'].value_counts()[True]
+
+    print tweets[tweets['relevant'] == True]['python'].value_counts()[True]
+    print tweets[tweets['relevant'] == True]['javascript'].value_counts()[True]
+    print tweets[tweets['relevant'] == True]['ruby'].value_counts()[True]
+
+    tweets_by_prg_lang = [tweets[tweets['relevant'] == True]['python'].value_counts()[True],
+                      tweets[tweets['relevant'] == True]['javascript'].value_counts()[True],
+                      tweets[tweets['relevant'] == True]['ruby'].value_counts()[True]]
+    x_pos = list(range(len(prg_langs)))
+    width = 0.8
+    fig, ax = plt.subplots()
+    plt.bar(x_pos, tweets_by_prg_lang, width,alpha=1,color='g')
+    ax.set_ylabel('Number of tweets', fontsize=15)
+    ax.set_title('Ranking: python vs. javascript vs. ruby (Relevant data)', fontsize=10, fontweight='bold')
+    ax.set_xticks([p + 0.4 * width for p in x_pos])
+    ax.set_xticklabels(prg_langs)
+    plt.grid()
+
+    tweets['link'] = tweets['text'].apply(lambda tweet: extract_link(tweet))
+
+    tweets_relevant = tweets[tweets['relevant'] == True]
+    tweets_relevant_with_link = tweets_relevant[tweets_relevant['link'] != '']
+
+    print tweets_relevant_with_link[tweets_relevant_with_link['python'] == True]['link']
+    print tweets_relevant_with_link[tweets_relevant_with_link['javascript'] == True]['link']
+    print tweets_relevant_with_link[tweets_relevant_with_link['ruby'] == True]['link']
+
+
 
 if __name__ == '__main__':
     main()
